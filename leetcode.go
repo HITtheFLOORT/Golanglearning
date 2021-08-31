630a23189037aa09b23794f8bac5151f89b97e0e
HITtheFLOORT <1425087554@qq.com>
2021-08-31 16:52:27 +0800

    Last commit

--- leetcode.go
+++ leetcode.go
@@ -1,7 +1,13 @@
 package main
 
 import "sort"
-
+func Factorial(a int)int{
+	if(a>0){
+		return Factorial(a-1)
+	}else{
+		return 1
+	}
+}
 func numRescueBoats(people []int, limit int) (ans int) {
 	sort.Ints(people)
 	light, heavy := 0, len(people)-1
@@ -15,5 +21,19 @@ func numRescueBoats(people []int, limit int) (ans int) {
 		ans++
 	}
 	return
+}
+
+func corpFlightBookings(bookings [][]int, n int) []int {
+	nums := make([]int, n)
+	for _, booking := range bookings {
+		nums[booking[0]-1] += booking[2]
+		if booking[1] < n {
+			nums[booking[1]] -= booking[2]
+		}
+	}
+	for i := 1; i < n; i++ {
+		nums[i] += nums[i-1]
+	}
+	return nums
 }
 
--- studygo/daytwo/0map/main.go
+++ studygo/daytwo/0map/main.go
@@ -1,9 +1,54 @@
 package main
 
 import "fmt"
-
+var a=func(x int)int{
+	return 1//函数内部不能声明有名字的函数
+}
+//闭包 python 的装饰器
+func f1(f func()){
+	f()
+}
+func f2(x,y int)  {
+	fmt.Println(x+y)
+}
+func f3(f2 func(n ,m int),n,m int)func()int{
+	tmp:=func() int {
+		f2(n,m)
+		return m+n;
+	}
+	return tmp
+}
+//func f3(f2 func(n ,m int),n,m int)func(){
+//	tmp:=func() {
+//		f2(n,m)
+//	}
+//	return tmp
+//}
+/*
+闭包是匿名函数与匿名函数所引用环境的组合。匿名函数有动态创建的特性，
+该特性使得匿名函数不用通过参数传递的方式，就可以直接引用外部的变量。
+这就类似于常规函数直接使用全局变量一样，个人理解为：匿名函数和它引用的变量以及环境，
+类似常规函数引用全局变量处于一个包的环境。
+ */
+func adder()func(a int)int{
+	x:=100
+	return func(a int) int {
+		return a+x
+	}
+}
 func main() {
 	m1:=make(map[string]int,10)
 	m2:='a'
-	fmt.Printf("%T%T\n",m1,m2)
+	fmt.Printf( "%T%T\n",m1,m2)
+
+	//立即函数 只执行一次
+	func(a,b int ){
+		fmt.Println(a+b)
+	}(100,200)
+
+	f3(f2,10,20)
+
+	ret:=adder()
+	ret2:=ret(100)
+	fmt.Println(ret2)
 }
